7c90ecaccbc73201236804007b7b56efa9bd9f1b
Jhanlyn Brita Dannuy <jdannuy@myseneca.ca>
2025-01-15 04:40:03 +0000

    completed lab

---
+++ lab1d.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Declare variables
+x = 10  # integer with value 10
+y = 2   # integer with value 2
+z = 5   # integer with value 5
+
+# Print result of the mathematical operation
+print(f"{x} + {y} * {z} = {x + y * z}")  # Uses PEMDAS rules
