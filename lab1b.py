e738f3eac3ab3fb22f18cf6bb05ca634611b12af
Jhanlyn Brita Dannuy <jdannuy@myseneca.ca>
2025-01-15 04:36:33 +0000

    completed lab

---
+++ lab1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Lab1b script for string objects
+
+# Define the name object
+name = 'Isaac'
+
+# Print the desired output
+print('How old are you ' + name + '?')
---
+++ lab1c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Lab1c script for integer objects
+
+# Define the name and age objects
+name = 'Isaac'
+age = 72
+
+# Print the desired output
+print(name + ' is ' + str(age) + ' years old!')
